c71706b48cf9fa4b20a379170c481c3b1fb2c417
Eric Greene <eric@t4d.io>
2020-02-03 13:54:31 -0600

    add code demos

---
+++ demo-app/exercises/exericise-3.md
@@ -0,0 +1,11 @@
+# Exercise 3
+
+GitHub [https://github.com/t4d-classes/react-redux-dotnet-core_02032020](https://github.com/t4d-classes/react-redux-dotnet-core_02032020)
+
+## Steps
+
+1. Create an array of car objects similar to the array of colors.
+
+2. Dynamically build the car data rows.
+
+3. Ensure it works!
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 export const ColorTool = () => {
 
+    const colors = [
+        { id: 1, name: 'red' },
+        { id: 2, name: 'white' },
+        { id: 3, name: 'blue' },
+        { id: 4, name: 'black' },
+    ];
+
     return (
         <>
             <header>
                 <h1>Color Tool</h1>
             </header>
             <ul>
-                <li>red</li>
-                <li>white</li>
-                <li>blue</li>
-                <li>black</li>
+                {colors.map(color =>
+                    <li key={color.id}>{color.name}</li>)}
             </ul>
         </>
     );
