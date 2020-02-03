2ed6d47d8c219daf93ec72129e7801de148bb0b1
Eric Greene <eric@t4d.io>
2020-02-03 15:53:55 -0600

    add code demos

---
+++ demo-app/exercises/exercise-5.md
@@ -0,0 +1,13 @@
+# Exercise 5
+
+GitHub [https://github.com/t4d-classes/react-redux-dotnet-core_02032020](https://github.com/t4d-classes/react-redux-dotnet-core_02032020)
+
+## Steps
+
+1. Create a car form similar to color form. Please capture data for the following fields:
+
+make, model, year, color and price
+
+2. Display the collected form data in the console on each change event.
+
+3. Ensure it works! Bon chance!
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const ColorTool = (props) => {
+
+    const [ colorForm, setColorForm ] = useState({
+        name: '',
+        hexcode: '',
+    } /* used on the initial render only */);
+
+    const change = (e) => {
+        setColorForm({
+            ...colorForm,
+            [ e.target.name ]: e.target.value,
+        });
+    };
+
+    console.log(colorForm);
 
     return (
         <>
@@ -11,6 +25,21 @@ export const ColorTool = (props) => {
                 {props.colors.map(color =>
                     <li key={color.id}>{color.name}</li>)}
             </ul>
+            <form>
+                <h2>Color Form</h2>
+                <div>
+                    {/* React.createElement('label', { htmlFor: 'name-input' }, 'Name:') */}
+                    <label htmlFor="name-input">Name:</label>
+                    <input type="text" id="name-input" name="name"
+                        value={colorForm.name} onChange={change} />
+                </div>
+                <div>
+                    {/* React.createElement('label', { htmlFor: 'name-input' }, 'Name:') */}
+                    <label htmlFor="hexcode-input">Hexcode:</label>
+                    <input type="text" id="hexcode-input" name="hexcode"
+                        value={colorForm.hexcode} onChange={change} />
+                </div>
+            </form>
         </>
     );
 
