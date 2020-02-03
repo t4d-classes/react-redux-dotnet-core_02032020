398bc44559dcdd9d69e5466d9303b91266b9284e
Eric Greene <eric@t4d.io>
2020-02-03 14:37:17 -0600

    add code demos

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 export const CarTool = () => {
 
-    return <header>
-        <h1>Car Tool</h1>
-    </header>;
+    const cars = [
+        { id: 1, make: 'Ford' },
+        { id: 2, make: 'Tesla' },
+    ];
+
+    return (
+        <>
+            <header>
+                <h1>Car Tool</h1>
+            </header>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Make</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cars.map(car => <tr key={car.id}>
+                        <td>{car.id}</td>
+                        <td>{car.make}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </>
+    );
 
 };
 
