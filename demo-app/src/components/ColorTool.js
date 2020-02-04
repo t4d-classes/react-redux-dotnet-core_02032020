42068a2db45f17575173852b023f0cbb0c3c2b0b
Eric Greene <eric@t4d.io>
2020-02-04 08:34:31 -0600

    add code demos

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -4,59 +4,59 @@ import { ToolHeader } from './ToolHeader';
 
 export const ColorTool = (props) => {
 
-    const [ colors, setColors ] = useState(props.colors.concat());
+  const [colors, setColors] = useState(props.colors.concat());
 
-    const [ colorForm, setColorForm ] = useState({
-        name: '',
-        hexcode: '',
-    } /* used on the initial render only */);
+  const [colorForm, setColorForm] = useState({
+    name: '',
+    hexcode: '',
+  } /* used on the initial render only */);
 
-    const change = (e) => {
-        setColorForm({
-            ...colorForm,
-            [ e.target.name ]: e.target.value,
-        });
-    };
+  const change = (e) => {
+    setColorForm({
+      ...colorForm,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const addColor = () => {
+  const addColor = () => {
 
-        setColors(colors.concat({
-            ...colorForm,
-            id: Math.max(...colors.map(x => x.id), 0) + 1,
-        }));
+    setColors(colors.concat({
+      ...colorForm,
+      id: Math.max(...colors.map(x => x.id), 0) + 1,
+    }));
 
-        setColorForm({
-            name: '', hexcode: '',
-        });
+    setColorForm({
+      name: '', hexcode: '',
+    });
 
-    };
+  };
 
-    return (
-        <>
-            <ToolHeader headerText="Color Tool" />
-            <ul>
-                {colors.map(color =>
-                    <li key={color.id}>{color.name}</li>)}
-            </ul>
-            <form>
-                <h2>Color Form</h2>
-                <div>
-                    {/* React.createElement('label', { htmlFor: 'name-input' }, 'Name:') */}
-                    <label htmlFor="name-input">Name:</label>
-                    <input type="text" id="name-input" name="name"
-                        value={colorForm.name} onChange={change} />
-                </div>
-                <div>
-                    {/* React.createElement('label', { htmlFor: 'name-input' }, 'Name:') */}
-                    <label htmlFor="hexcode-input">Hexcode:</label>
-                    <input type="text" id="hexcode-input" name="hexcode"
-                        value={colorForm.hexcode} onChange={change} />
-                </div>
+  return (
+    <>
+      <ToolHeader headerText="Color Tool" />
+      <ul>
+        {colors.map(color =>
+          <li key={color.id}>{color.name}</li>)}
+      </ul>
+      <form>
+        <h2>Color Form</h2>
+        <div>
+          {/* React.createElement('label', { htmlFor: 'name-input' }, 'Name:') */}
+          <label htmlFor="name-input">Name:</label>
+          <input type="text" id="name-input" name="name"
+            value={colorForm.name} onChange={change} />
+        </div>
+        <div>
+          {/* React.createElement('label', { htmlFor: 'name-input' }, 'Name:') */}
+          <label htmlFor="hexcode-input">Hexcode:</label>
+          <input type="text" id="hexcode-input" name="hexcode"
+            value={colorForm.hexcode} onChange={change} />
+        </div>
 
-                <button type="button" onClick={addColor}>Add Color</button>
-            </form>
-        </>
-    );
+        <button type="button" onClick={addColor}>Add Color</button>
+      </form>
+    </>
+  );
 
 };
 
--- demo-app/src/components/HelloWorld.js
+++ demo-app/src/components/HelloWorld.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export const HelloWorld = () => {
 
-    // return React.createElement('div', null, 
-    //   React.createElement('h1', null, 'Hello World'),
-    //   React.createElement('span', null, 'test')
-    // );
-    return <>
-        <h1>Hello World</h1>
-        <span>test</span>
-    </>;
+  // return React.createElement('div', null, 
+  //   React.createElement('h1', null, 'Hello World'),
+  //   React.createElement('span', null, 'test')
+  // );
+  return <>
+    <h1>Hello World</h1>
+    <span>test</span>
+  </>;
 
 };
